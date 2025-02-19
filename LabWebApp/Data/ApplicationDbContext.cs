1af5d2af28369719c99840096495870dbfc9b04f
Shin417 <shintaroasoemail@gmail.com>
2025-02-19 12:51:17 -0500

    installed EF

--- LabWebApp/Data/ApplicationDbContext.cs
+++ LabWebApp/Data/ApplicationDbContext.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using LabWebApp.Models;
 
-namespace LabWebApp.Data;
-
-public class ApplicationDbContext : IdentityDbContext
+namespace LabWebApp.Data
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
     }
 }
+
