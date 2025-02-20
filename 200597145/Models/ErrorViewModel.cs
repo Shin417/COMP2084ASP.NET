deebe28387265e40843eb68a75b642acb6dc9fe9
Shin417 <shintaroasoemail@gmail.com>
2025-02-19 20:20:16 -0500

    final

---
+++ 200597145/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _200597145.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 200597145/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _200597145.Models
+{
+    public class Student
+    {
+        [Required]
+        [Display(Name = "Student ID")]
+        public int StudentId { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+    }
+}
