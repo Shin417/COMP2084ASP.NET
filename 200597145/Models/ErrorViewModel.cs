782aa27de55b5598cc670f07d7c79c151b95d5d6
Shin417 <shintaroasoemail@gmail.com>
2025-03-28 05:12:17 -0400

    Revert "final"
    This reverts commit deebe28387265e40843eb68a75b642acb6dc9fe9.

--- 200597145/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _200597145.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
