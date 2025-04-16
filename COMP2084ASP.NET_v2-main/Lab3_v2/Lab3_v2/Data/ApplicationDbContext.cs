780789a9dd62b0c879c3c21495dad0e94bbeb4fb
Shin417 <shintaroasoemail@gmail.com>
2025-04-16 13:56:05 -0400

    Add the last lab codes

---
+++ COMP2084ASP.NET_v2-main/Lab3_v2/Lab3_v2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Lab3_v2.Models;
+
+namespace Lab3_v2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
